6cd936db265068df7be4f90d09edffa98621bfb0
Arafat Ali <aravatt22@gmail.com>
2025-03-23 13:37:41 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-[#4DBAF8FF] p-4"'>Welcome</h1>
+    </>
+  )
+}
+
+export default App
